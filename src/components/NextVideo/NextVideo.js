9d3f038e48438d64ed964183f14479c2e32a5856
Rebecca Brown <rebecca.brown@msn.com>
2022-08-21 21:16:18 -0400

    finish styling comments and add elipsis function

--- src/components/NextVideo/NextVideo.js
+++ src/components/NextVideo/NextVideo.js
@@ -1,5 +1,23 @@
 import "./NextVideo.scss";
 
+function addElipsis(title) {
+    for(let i=32; i<title.length; i++){
+        if (title[i] === " ") {
+            return title.slice(0, i) + "...";
+            }
+    }
+    }
+
+function checkTitleLength(title) {
+    for (let i=0; i<title.length; i++) {
+        if (title.length < 40) {
+            return title;
+        } else {
+            return addElipsis(title);
+        }
+    }
+}
+
 function NextVideo({ video, handleCurrentVideo }) {
     return (
         <section onClick={() => {
@@ -8,7 +26,7 @@ function NextVideo({ video, handleCurrentVideo }) {
         }} className="next-video">
             <img src={video.image} alt="next video placeholder" className="next-video__image" />
             <div className="next-video__content">
-                <h2 className="next-video__title">{video.title}</h2>
+                <h2 className="next-video__title">{checkTitleLength(video.title)}</h2>
                 <span className="next-video__channel">{video.channel}</span>
             </div>
         </section>
