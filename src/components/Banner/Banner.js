4c4775544665f4b786a60bbef038e655ba8e741d
Rebecca Brown <rebecca.brown@msn.com>
2022-08-25 22:52:06 -0400

    add underline to upload form

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,15 +1,21 @@
 import "./Banner.scss";
 import { Link } from "react-router-dom";
+import Logo from "../../assets/logos/BrainFlix-logo.svg";
 
 
 function Banner() {
     return (
         <div className="banner">
-            <div className="banner__content">
-                <span className="banner__text">Your video uploaded sucessfully!</span>
-                <Link to="/" className="banner__button">
-                    Go to videos
+            <div className="banner__container">
+                <Link to="/" className="banner__link">
+                    <img src={Logo} alt="brainflix logo" className="banner__logo" />
                 </Link>
+                <div className="banner__content">
+                    <span className="banner__text">Your video uploaded sucessfully!</span>
+                    <Link to="/" className="banner__button">
+                        Go to videos
+                    </Link>
+                </div>
             </div>
         </div>
     )
--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -9,7 +9,6 @@ import { useState } from "react";
 
 function Upload() {
 
-
     const [uploadStatus, setUploadStatus] = useState(false);
 
     const submitHandler = (event) => {
@@ -17,38 +16,42 @@ function Upload() {
         setUploadStatus(true);
     }
 
-
-
     return (
         <>
             <section className="upload">
                 <h1 className="upload__title">Upload Video</h1>
-                <div className="upload__content">
-                    <div className="upload__media">
-                        <h2 className="upload__subheading">Video Thumbnail</h2>
-                        <img src={uploadImage} alt="video upload preview" className="upload__image" />
+
+                <form className="upload__form"
+                    onSubmit={submitHandler} >
+                    <div className="upload__container">
+                        <div className="upload__media">
+                            <h2 className="upload__subheading">Video Thumbnail</h2>
+                            <img src={uploadImage} alt="video upload preview" className="upload__image" />
+                        </div>
+                        <div className="upload__content">
+                            <div className="upload__formfields">
+                                <label htmlFor="title" className="upload__subheading">Title your video</label>
+                                <input type="text" name="title" id="title" placeholder="Add a title to your video" className="upload__input--title" />
+                                <label htmlFor="description" className="upload__subheading">Add a video description</label>
+                                <textarea name="description" id="description" className="upload__input--description" placeholder="Add a description to your video"></textarea>
+                            </div>
+                        </div>
                     </div>
-                    <form className="upload__form"
-                        onSubmit={submitHandler} >
-                        <div className="upload__formfields">
-                            <label htmlFor="title" className="upload__subheading">Title your video</label>
-                            <input type="text" name="title" id="title" placeholder="Add a title to your video" className="upload__input--title" />
-                            <label htmlFor="description" className="upload__subheading">Add a video description</label>
-                            <textarea name="description" id="description" className="upload__input--description" placeholder="Add a description to your video"></textarea>
-                        </div>
-                        <div className="upload__buttons">
-                            <input
-                                type="submit"
-                                name="submit"
-                                value="Publish"
-                                className="upload__button"
-                            />
-                            <Link to="/" className="upload__link">
-                                Cancel
-                            </Link>
-                        </div>
-                    </form>
-                </div>
+                    <div className="upload__buttons">
+                        <input
+                            type="submit"
+                            name="submit"
+                            value="Publish"
+                            className="upload__button"
+                        />
+
+                        <Link to="/" className="upload__link">
+                            Cancel
+                        </Link>
+                    </div>
+
+                </form>
+
             </section >
             {(uploadStatus === true) ? <Banner className="banner" /> : console.log("no banner")}
             {/* // <Banner className="banner" /> */}
